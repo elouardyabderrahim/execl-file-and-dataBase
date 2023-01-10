4897dc46ae1180ae60b88d17794c80e30168dde0
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-01-10 10:24:27 +0100

    setting app the project by adding the necessary dependencies

--- exportImportexcel/src/main/java/com/exportImportexcel/ExportImportexcelApplication.java
+++ exportImportexcel/src/main/java/com/exportImportexcel/ExportImportexcelApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExportImportexcelApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ExportImportexcelApplication.class, args);
 	}
