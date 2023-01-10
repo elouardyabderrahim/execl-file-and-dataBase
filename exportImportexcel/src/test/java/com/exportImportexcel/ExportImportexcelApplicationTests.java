57c59fa4ab1effc53c602c7b4df52430f0bba5b0
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-01-10 10:09:54 +0100

    creating Spring boot project

---
+++ exportImportexcel/src/test/java/com/exportImportexcel/ExportImportexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exportImportexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportImportexcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
