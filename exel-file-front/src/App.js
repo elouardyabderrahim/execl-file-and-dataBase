bcc73f2e32c2fc1090e377de74536fa18f84a106
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-01-18 11:06:33 +0100

    feature:desplaying data in front-end

--- exel-file-front/src/App.js
+++ exel-file-front/src/App.js
@@ -1,8 +1,18 @@
-
+import { useEffect } from "react";
+import { getAll } from "./api/excel.api";
 import "./App.css";
+import EmployeeList from "./components/employee/EmployeeList";
 
 function App() {
-  return <div> excel file</div>;
+
+  return (
+    <>
+      <h1>excel file</h1>
+      <div>
+        <EmployeeList />
+      </div>
+    </>
+  );
 }
 
 export default App;
